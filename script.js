a8c5b0bce9cc1bc5ef795bebe887c308f1c59e65
leotachini <96491016+leotachini@users.noreply.github.com>
2023-10-25 14:00:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ function calcularIMC() {
     let weight = parseFloat(document.getElementById('peso').value);
     let height = parseFloat(document.getElementById('altura').value);
   
- 
-  
     let imc = weight / ((height / 100) * (height / 100));
     let resultDiv = document.getElementById('result');
     resultDiv.innerHTML = 'Seu calculo de IMC resultou em: ' + imc.toFixed(1);
     document.addEventListener("keypress", calcularIMC);
+    document.getElementById("slider").value = imc.toFixed(0);
   
     if (imc < 18.5) {
       document.body.style.backgroundColor = '#74ff57'; 
@@ -19,8 +18,9 @@ function calcularIMC() {
       document.body.style.backgroundColor = '#ffffb3'; 
       document.getElementById("diagnostico").innerHTML = 'Você está acima do peso ideal';
     } else {
-      document.body.style.backgroundColor = '#a50000'; 
+      document.body.style.backgroundColor = '#730000'; 
       document.getElementById("diagnostico").innerHTML = 'Você está com algum grau de obesidade';
     }
+    
   }
   
